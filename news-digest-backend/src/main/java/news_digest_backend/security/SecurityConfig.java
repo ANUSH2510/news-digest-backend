8248e94b070861f30a4456d5b37567af2c60cfd4
ANUSH2510 <anush003007@gmail.com>
2026-08-21 07:19:26 +0530

    Update SecurityConfig.java

--- news-digest-backend/src/main/java/news_digest_backend/security/SecurityConfig.java
+++ news-digest-backend/src/main/java/news_digest_backend/security/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**","/api/digest/trigger").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtFilter,
                         UsernamePasswordAuthenticationFilter.class);
